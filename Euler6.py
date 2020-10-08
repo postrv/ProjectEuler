1f0624d0bf8822a94386b5261ae86159df838263
postrv <71533421+postrv@users.noreply.github.com>
2020-10-08 17:01:33 +0200

    simpified function - not quite a oneliner

--- Euler6.py
+++ Euler6.py
@@ -8,22 +8,16 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. """
 
-num = 101#  adjusted up to avoid off-by-one error but need to find a more elegant way of handling this in ranges
+import time
 
-def sum_squares(x):
-    ans1 = 0
-    for i in range(x):
-        ans1 += i**2
-    return ans1
+t0 = time.time()
+num = 101  # adjusted up to avoid off-by-one error but need to find a more elegant way of handling this in ranges
+s_sum = 0
+sum_squares = 0
 
-
-
-def square_sums(y):
-    ans2 = 0
-    for n in range(y):
-        ans2 += n
-    return (ans2 ** 2)
-
-print(sum_squares(num))
-print(square_sums(num))
-print(abs(sum_squares(num) - square_sums(num)))
+for x in range(num):
+    sum_squares += x ** 2
+    s_sum += x
+print(s_sum ** 2 - sum_squares)
+t1 = time.time()
+print(t1 - t0)
